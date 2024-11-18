50458ee460f7935b423fe038192316373a8dbb41
sehun.gim <sehoongim@gmail.com>
2024-11-18 20:45:42 +0900

    텍스트 포맷 work experience를 파싱할 수 있다

---
+++ lib/preproc.rb
@@ -0,0 +1,80 @@
+class Preproc
+
+  def company_nm?(l)
+    l =~ /^\s*(company_nm:)/
+  end
+  def work_from_to?(l)
+    l =~ /^\s*(work_from_to:)/
+  end
+  def solved?(l)
+    l =~ /^\s*(solved:)/
+  end
+  def what?(l)
+    l =~ /^\s*(what:)/
+  end
+  def details?(l)
+    l =~ /^\s*(details:)/
+  end
+
+  def split_by_company work_exp
+    lines = work_exp.split("\n")
+
+    ranges = []
+    lines.select {|l| company_nm?(l)}.each do |l|
+      ranges << lines.find_index(l)
+    end
+    res = []
+    ranges.each.with_index do |range, idx|
+      if (idx == ranges.length - 1)
+        res << lines[range..(lines.length - 1)]
+        return res
+      else
+        res << lines[range...ranges[idx + 1]]
+      end
+    end
+  end
+
+  def group_by_company(work_exp)
+    lines = work_exp.split("\n")
+
+    group = {}
+    solve = []
+    lines.each do |l|
+      case
+      when company_nm?(l) then
+        group = {}
+        group[:company_nm] = l.split(":")[1].strip
+      when work_from_to?(l) then
+        group[:work_from_to] = l.split(":")[1].strip
+      else
+        solve << l
+      end
+    end
+    group[:solved] = group_solved solve.join("\n") if solve != []
+    group
+  end
+
+  def group_solved sol
+    lines = sol.split("\n")
+
+    solved_tasks = {}
+    solved_task_nm = nil
+    solve_what = nil
+    lines.each do |l|
+      case
+      when solved?(l) then
+        solved_task_nm = l.split(":")[1].strip
+        solved_tasks[solved_task_nm] = []
+      when what?(l) then
+        solve_what = l.split(":").size > 1 ? l.split(":")[1].strip : :EMPTY_WHAT
+        solved_tasks[solved_task_nm] << {solve_what => []}
+      when details?(l) then
+        ""
+      else
+        idx = solved_tasks[solved_task_nm].find_index {|e| e[solve_what] != nil}
+        solved_tasks[solved_task_nm][idx][solve_what] << l.strip
+      end
+    end
+    solved_tasks
+  end
+end
---
+++ spec/preproc_spec.rb
@@ -0,0 +1,223 @@
+require_relative "../lib/preproc"
+
+describe Preproc do
+  RSpec.configure do |config|
+    config.filter_run_when_matching(focus: true)
+    config.example_status_persistence_file_path = 'spec/pass_fail_history'
+  end
+
+  TEST_DATA_DIR = File.join(File.dirname(__FILE__), *%w[data])
+
+  before(:each) do
+    @pp = Preproc.new
+  end
+
+  it "여러 회사명, 일한 기간을 파싱할 수 있다" do
+    src_path_sp = File.join(TEST_DATA_DIR, *%w[company_2])
+
+    expected = [
+      ["company_nm: c1", "work_from_to: f ~ t", ""],
+      ["company_nm: c2", "work_from_to: f ~ t"]
+    ]
+
+    expect(@pp.split_by_company(File.read(src_path_sp))).to eq(expected)
+  end
+
+  it "회사명, 일한 기간을 파싱할 수 있다" do
+    src_path_sp = File.join(TEST_DATA_DIR, *%w[company_1])
+
+    expected = {
+      :company_nm => "c1",
+      :work_from_to => "f ~ t"
+    }
+
+    expect(@pp.group_by_company(File.read(src_path_sp))).to eq(expected)
+  end
+
+  it "한 일에 대해 작성한 내용을 모을 수 있다" do
+    src_path_sp = File.join(TEST_DATA_DIR, *%w[solve_1])
+
+    expected = {
+      "s" => [
+        {"w" => ["d1", "d2", "d3"]}
+      ]
+    }
+
+    expect(@pp.group_solved(File.read(src_path_sp))).to eq(expected)
+  end
+
+  it "한 일에 대해 작성한 여러 내용을 모을 수 있다" do
+    src_path_sp = File.join(TEST_DATA_DIR, *%w[solve_2])
+
+    expected = {
+      "s" => [
+        { "w" => ["d1", "d2", "d3"] },
+        { "w2" => ["d4", "d5", "d6"] },
+      ]
+    }
+
+    expect(@pp.group_solved(File.read(src_path_sp))).to eq(expected)
+  end
+
+  it "여러 일에 대해 작성한 내용을 모을 수 있다" do
+    src_path_sp = File.join(TEST_DATA_DIR, *%w[solves_1])
+
+    expected = {
+      "s" => [
+        { "w" => ["d1", "d2", "d3"] }
+      ],
+      "s2" => [
+        { "w" => ["d1", "d2", "d3"] }
+      ]
+    }
+
+    expect(@pp.group_solved(File.read(src_path_sp))).to eq(expected)
+  end
+
+  it "여러 일에 대해 작성한 여러 내용을 모을 수 있다" do
+    src_path_sp = File.join(TEST_DATA_DIR, *%w[solves_2])
+
+    expected = {
+      "s" => [
+        { "w" => ["d1", "d2", "d3"] },
+        { "w2" => ["d4", "d5", "d6"] }
+      ],
+      "s2" => [
+        { "w" => ["d1", "d2", "d3"] },
+        { "w3" => ["d4", "d5", "d6"] }
+      ]
+    }
+
+    expect(@pp.group_solved(File.read(src_path_sp))).to eq(expected)
+  end
+
+  it "회사명, 일한 기간, 한 일을 파싱할 수 있다" do
+    src_path_sp = File.join(TEST_DATA_DIR, *%w[company_with_solve_1])
+
+    expected = {
+      :company_nm => "c1",
+      :work_from_to => "f ~ t",
+      :solved => {
+        "s" => [
+          { "w" => ["d1", "d2", "d3"] }
+        ]
+      }
+    }
+
+    expect(@pp.group_by_company(File.read(src_path_sp))).to eq(expected)
+  end
+
+  it "회사명, 일한 기간, 한 일을 파싱할 수 있다" do
+    src_path_sp = File.join(TEST_DATA_DIR, *%w[company_with_solve_1])
+
+    expected = {
+      :company_nm => "c1",
+      :work_from_to => "f ~ t",
+      :solved => {
+        "s" => [
+          { "w" => ["d1", "d2", "d3"] }
+        ]
+      }
+    }
+
+    expect(@pp.group_by_company(File.read(src_path_sp))).to eq(expected)
+  end
+
+  it "회사명, 일한 기간, 한 일 여러 개를 파싱할 수 있다" do
+    src_path_sp = File.join(TEST_DATA_DIR, *%w[company_with_solve_2])
+
+    expected = {
+      :company_nm => "c1",
+      :work_from_to => "f ~ t",
+      :solved => {
+        "s" => [
+          { "w" => ["d1", "d2", "d3"] },
+          { "w2" => ["d4", "d5", "d6"] }
+        ]
+      }
+    }
+
+    expect(@pp.group_by_company(File.read(src_path_sp))).to eq(expected)
+  end
+
+  it "what이 없는 경우 기본값을 설정한다" do
+    src_path_sp = File.join(TEST_DATA_DIR, *%w[no_what])
+
+    expected = {
+      :solved => {
+        "s" => [
+          { :EMPTY_WHAT => ["d1", "d2", "d3"] },
+        ]
+      }
+    }
+
+    expect(@pp.group_by_company(File.read(src_path_sp))).to eq(expected)
+  end
+
+  it "actual" do
+    src_path_sp = File.join(TEST_DATA_DIR, *%w[actual])
+
+    expected = {
+      :company_nm => "(주) 두잇",
+      :work_from_to => "2021.05 ~ 2022.05",
+      :solved => {
+        "건축 분야 알고리즘 기반 솔루션 개발 참여" => [
+          {
+            "트레일러 3D 모델을 보여주는 혼합현실 애플리케이션 개발 (C#, Unity, MRTK)" => [
+              "수치값으로만 확인할 수 있었던 알고리즘 연산 결과를 3D 형태로 시각화해 부재 정보를 편하게 확인",
+              "기존에 Unity 기반 2D 방식으로 구현되었으나, 시각적 제한을 개선하기 위해 혼합현실 기술을 도입",
+              "사용자 편의성을 고려해 렌더링되는 트레일러의 위치와 조작 방식을 개선하고, 작업자가 현장에서 실제 트레일러와 3D 모델을 비교해볼 수 있도록 3D 모델 확대 기능을 구현"
+            ]
+          },
+          {
+            "캐드 소프트웨어 플러그인 개발 (C#, Revit API)" => [
+              "알고리즘 연산에 필요한 입력값을 캐드 프로그램에서 입력하고 추출해 쓸 수 있도록 도와주는 플러그인",
+              "캐드 외부에서 부재의 3D 모델을 활용할 수 있도록 건축 부재의 3D 모델링 정보 추출 기능을 구현",
+              "기존 건축 설계 과정(설계도 제작) 중에 알고리즘 데이터 입력을 가능하도록 해 사용성을 개선"
+            ]
+          }
+        ]
+      }
+    }
+
+    expect(@pp.group_by_company(File.read(src_path_sp))).to eq(expected)
+  end
+
+  it "real actual" do
+    src_path_sp = File.join(TEST_DATA_DIR, *%w[real_actual])
+
+    expected = {
+      :company_nm => "(주) 두잇",
+      :work_from_to => "2021.05 ~ 2022.05",
+      :solved => {
+        "건축 분야 알고리즘 기반 솔루션 개발 참여" => [
+          {
+            "트레일러 3D 모델을 보여주는 혼합현실 애플리케이션 개발 (C#, Unity, MRTK)" => [
+              "수치값으로만 확인할 수 있었던 알고리즘 연산 결과를 3D 형태로 시각화해 부재 정보를 편하게 확인",
+              "기존에 Unity 기반 2D 방식으로 구현되었으나, 시각적 제한을 개선하기 위해 혼합현실 기술을 도입",
+              "사용자 편의성을 고려해 렌더링되는 트레일러의 위치와 조작 방식을 개선하고, 작업자가 현장에서 실제 트레일러와 3D 모델을 비교해볼 수 있도록 3D 모델 확대 기능을 구현"
+            ]
+          },
+          {
+            "캐드 소프트웨어 플러그인 개발 (C#, Revit API)" => [
+              "알고리즘 연산에 필요한 입력값을 캐드 프로그램에서 입력하고 추출해 쓸 수 있도록 도와주는 플러그인",
+              "캐드 외부에서 부재의 3D 모델을 활용할 수 있도록 건축 부재의 3D 모델링 정보 추출 기능을 구현",
+              "기존 건축 설계 과정(설계도 제작) 중에 알고리즘 데이터 입력을 가능하도록 해 사용성을 개선"
+            ]
+          }
+        ],
+        "사내 버전 관리 문화 도입" => [
+          {
+            :EMPTY_WHAT => [
+              "사내 개발 인력 증가로 개별 작업한 프로젝트 소스 코드의 통합 문제가 우려되어 버전 관리를 도입",
+              "GitLab 서버 설치, 유지 보수 담당 및 사내 팀원에게 Git을 통한 형상 관리 세미나 진행",
+              "여러 인원이 참여하는 프로젝트에서 소스 코드 통합 문제 해결에 기여"
+            ]
+          }
+        ]
+      }
+    }
+
+    expect(@pp.group_by_company(File.read(src_path_sp))).to eq(expected)
+  end
+end
