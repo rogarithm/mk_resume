6e29f2d33400f241710621bbf909ea23d236e1d4
sehoon gim <sehoongim@gmail.com>
2025-03-10 18:15:53 +0900

    스크립트를 역할별 객체로 분리한다 (#8)
    - 리팩토링 도중 동작이 깨지는지 확인할 수 있는 인수 테스트를 구현한다
    - 스크립트 내의 로직을 분리할 수 있도록, 메서드 요청 객체가 명시적으로 드러나도록 바꾼다
    - 스크립트 내의 연산을 연관성 있는 것끼리 구분지어서 적절한 객체로 옮긴다
    - 패키지 구조 다듬는다

--- lib/mk_resume.rb
+++ lib/mk_resume.rb
@@ -1,282 +1,353 @@
 require 'prawn'
 require 'prawn/measurement_extensions'
-require_relative 'preproc'
+require_relative 'mk_resume/preproc'
+require_relative 'mk_resume/font_manager'
+require_relative 'mk_resume/document_writer'
+require_relative 'mk_resume/layout_arranger'
 
 # 플레인 텍스트 형식으로 적은 이력서를 pdf로 변환하기 위한 스크립트
 
-def load_font
-  Prawn::Font::AFM.hide_m17n_warning = true
-  font_families.update(
-    "NotoSans" => {
-      normal: "./fonts/NotoSansKR-Regular.ttf",
-      bold: "./fonts/NotoSansKR-Bold.ttf"
-    }
-  )
-  font "NotoSans"
-end
+class ResumePrinter
+  def initialize
+    @layout_arranger = MkResume::LayoutArranger.new
+    @font_manager = MkResume::FontManager.new
+    @doc_writer = MkResume::DocumentWriter.new
+    @preproc = MkResume::Preproc.new
+  end
 
-def draw_horizontal_line
-  stroke_horizontal_rule
-  move_down 9.5
-end
+  def run(relative_path)
+    link_style = "<color rgb='888888'><u>%s</u></color>"
+    Prawn::Document.generate(
+      "output.pdf",
+      page_size: "A4",
+      margin: [
+        @layout_arranger.find_margin_size(:top),
+        @layout_arranger.find_margin_size(:right),
+        @layout_arranger.find_margin_size(:bottom),
+        @layout_arranger.find_margin_size(:left)
+      ]
+    ) do |doc|
 
-def space_after_list_item
-  move_down 2
-end
+      @font_manager.load_font(doc)
 
-def space_after_paragraph
-  move_down 14.5
-end
+      ["personal_info"].each do |heading|
+        personal_info = File.readlines(File.join(File.dirname(__FILE__), *relative_path, *%W[personalInfo])).map(&:chomp)
 
-def draw_heading heading, font_size
-  draw_horizontal_line
-  line_height = 1.45
-  text(
-    heading[:text],
-    size: font_size[:heading],
-    style: :bold,
-    leading: line_height * font_size[:heading]
-  )
-end
-
-DOC_MARGIN = {:top => 2.cm, :right => 3.05.cm, :bottom => 2.cm, :left => 3.05.cm}
-FONT_SIZE = {:name => 11, :channel => 10, :heading => 9.5, :body => 9.5}
-
-def run(relative_path)
-  link_style = "<color rgb='888888'><u>%s</u></color>"
-
-  Prawn::Document.generate(
-    "output.pdf",
-    page_size: "A4",
-    margin: [DOC_MARGIN[:top], DOC_MARGIN[:right], DOC_MARGIN[:bottom], DOC_MARGIN[:left]]
-  ) do
-
-    load_font
-
-    ["personal_info"].each do |heading|
-      personal_info = File.readlines(File.join(File.dirname(__FILE__), *relative_path, *%W[personalInfo])).map(&:chomp)
-
-      text(
-        personal_info[0],
-        size: FONT_SIZE[:name],
-        style: :bold,
-        leading: 8
-      )
-
-      personal_info[1..2].each do |item|
-        text(
-          item,
-          size: FONT_SIZE[:channel],
-          leading: 5,
+        @doc_writer.write_text(
+          doc,
+          personal_info[0],
+          {
+            size: @font_manager.find_font_size(:name),
+            style: :bold,
+            leading: 8
+          }
         )
-      end
 
-      text(
-        link_style % personal_info[3],
-        size: FONT_SIZE[:channel],
-        leading: 5,
-        inline_format: true
-      )
-
-      # blog 링크
-      text(
-        link_style % personal_info[4],
-        size: FONT_SIZE[:channel],
-        leading: 5,
-        inline_format: true
-      )
-    end
-
-    space_after_paragraph
-
-    [{level: 4, text: "Introduction"}].each do |heading|
-      draw_heading(heading, FONT_SIZE)
-      intro_info = File.readlines(File.join(File.dirname(__FILE__), *relative_path, *%W[introduction])).map(&:chomp)
-
-      intro_info.each do |item|
-        indent(width_of("- ")) do
-          text(
-            "- #{item}",
-            size: FONT_SIZE[:body],
-            leading: 6,
-            indent_paragraphs: 0
+        personal_info[1..2].each do |item|
+          @doc_writer.write_text(
+            doc,
+            item,
+            {
+              size: @font_manager.find_font_size(:channel),
+              leading: 5
+            }
           )
         end
 
-        space_after_list_item
-      end
-    end
-
-    space_after_paragraph
-
-    [{ level: 4, text: "Work Experience" }].each do |heading|
-      draw_heading(heading, FONT_SIZE)
-
-      pp = Preproc.new
-      work_info = []
-      wis = pp.split_by_company(File.read(File.join(File.dirname(__FILE__), *relative_path, *%W[workExperience])))
-      wis.each do |wi|
-        work_info << pp.group_by_company(wi.join("\n"))
-      end
-
-      work_info.each.with_index do |wi, idx|
-        text(
-          wi[:company_nm],
-          size: FONT_SIZE[:body],
-          leading: 6,
-          indent_paragraphs: 0
+        @doc_writer.write_text(
+          doc,
+          link_style % personal_info[3],
+          {
+            size: @font_manager.find_font_size(:channel),
+            leading: 5,
+            inline_format: true
+          }
         )
-        space_after_list_item
-        text(
-          "사용기술: #{wi[:skill_set]}",
-          size: FONT_SIZE[:body],
-          leading: 12,
-          indent_paragraphs: 0
-        ) if wi[:skill_set]
-        space_after_list_item if wi[:skill_set]
 
-        wi[:project].keys.each do |solve|
-          text(
-            solve,
-            size: FONT_SIZE[:body],
-            leading: 6,
-            indent_paragraphs: 0
+        # blog 링크
+        @doc_writer.write_text(
+          doc,
+          link_style % personal_info[4],
+          {
+            size: @font_manager.find_font_size(:channel),
+            leading: 5,
+            inline_format: true
+          }
+        )
+      end
+
+      @layout_arranger.v_space(doc, 14.5)
+
+      [{level: 4, text: "Introduction"}].each do |heading|
+        @layout_arranger.draw_horizontal_rule(doc)
+        @layout_arranger.v_space(doc, 9.5)
+        line_height = 1.45
+        @doc_writer.write_text(
+          doc,
+          heading[:text],
+          {
+            size: @font_manager.find_font_size(:heading),
+            style: :bold,
+            leading: line_height * @font_manager.find_font_size(:heading)
+          }
+        )
+        intro_info = File.readlines(File.join(File.dirname(__FILE__), *relative_path, *%W[introduction])).map(&:chomp)
+
+        intro_info.each do |item|
+          @doc_writer.indent(doc, doc.width_of("- ")) do
+            @doc_writer.write_text(
+              doc,
+              "- #{item}",
+              {
+                size: @font_manager.find_font_size(:body),
+                leading: 6,
+                indent_paragraphs: 0
+              }
+            )
+          end
+
+          @layout_arranger.v_space(doc, 2)
+        end
+      end
+
+      @layout_arranger.v_space(doc, 14.5)
+
+      [{ level: 4, text: "Work Experience" }].each do |heading|
+        @layout_arranger.draw_horizontal_rule(doc)
+        @layout_arranger.v_space(doc, 9.5)
+        line_height = 1.45
+        @doc_writer.write_text(
+          doc,
+          heading[:text],
+          {
+            size: @font_manager.find_font_size(:heading),
+            style: :bold,
+            leading: line_height * @font_manager.find_font_size(:heading)
+          }
+        )
+
+        work_info = []
+        wis = @preproc.split_by_company(File.read(File.join(File.dirname(__FILE__), *relative_path, *%W[workExperience])))
+        wis.each do |wi|
+          work_info << @preproc.group_by_company(wi.join("\n"))
+        end
+
+        work_info.each.with_index do |wi, idx|
+          @doc_writer.write_text(
+            doc,
+            wi[:company_nm],
+            {
+              size: @font_manager.find_font_size(:body),
+              leading: 6,
+              indent_paragraphs: 0
+            }
+          )
+          @layout_arranger.v_space(doc, 2)
+          @doc_writer.write_text(
+            doc,
+            "사용기술: #{wi[:skill_set]}",
+            {
+              size: @font_manager.find_font_size(:body),
+              leading: 12,
+              indent_paragraphs: 0
+            }
+          ) if wi[:skill_set]
+          @layout_arranger.v_space(doc, 2) if wi[:skill_set]
+
+          wi[:project].keys.each do |solve|
+            @doc_writer.write_text(
+              doc,
+              solve,
+              {
+                size: @font_manager.find_font_size(:body),
+                leading: 6,
+                indent_paragraphs: 0
+              }
+            )
+
+            what_n_details_list = wi[:project][solve]
+            what_n_details_list.each do |what_n_details|
+              what_n_details.each_key {|what|
+                @doc_writer.indent(doc, doc.width_of("      ")) do
+                  @doc_writer.write_text(
+                    doc,
+                    what,
+                    {
+                      size: @font_manager.find_font_size(:body),
+                      leading: 6,
+                      indent_paragraphs: 0
+                    }
+                  )
+                end if what != :EMPTY_WHAT
+                details = what_n_details[what]
+                details.each do |detail_item|
+                  @doc_writer.indent(doc, doc.width_of("      ")) do
+                    @doc_writer.write_text(
+                      doc,
+                      "- #{detail_item}",
+                      {
+                        size: @font_manager.find_font_size(:body),
+                        leading: 6,
+                        indent_paragraphs: 0
+                      }
+                    )
+                  end
+                  @layout_arranger.v_space(doc, 2)
+                end
+                @layout_arranger.v_space(doc, 2)
+                @layout_arranger.v_space(doc, 2)
+                @layout_arranger.v_space(doc, 2)
+              }
+            end
+          end
+        end
+      end
+
+      @layout_arranger.v_space(doc, 2)
+      @layout_arranger.v_space(doc, 14.5)
+
+      [{ level: 4, text: "Side Project" }].each do |heading|
+        @layout_arranger.draw_horizontal_rule(doc)
+        @layout_arranger.v_space(doc, 9.5)
+        line_height = 1.45
+        @doc_writer.write_text(
+          doc,
+          heading[:text],
+          {
+            size: @font_manager.find_font_size(:heading),
+            style: :bold,
+            leading: line_height * @font_manager.find_font_size(:heading)
+          }
+        )
+
+        side_project_info = []
+        wis = @preproc.split_by_company(File.read(File.join(File.dirname(__FILE__), *relative_path, *%W[sideProject])))
+        wis.each do |wi|
+          side_project_info << @preproc.group_by_company(wi.join("\n"))
+        end
+
+        side_project_info.each do |spi|
+          spi[:project].keys.each do |project|
+
+            if project.match(/<link href='([^']*)'>([^<]*)<\/link>/)
+              link_url = Regexp.last_match(1)
+              link_text = Regexp.last_match(2)
+
+              @doc_writer.write_formatted_text(
+                doc,
+                [
+                  { text: spi[:company_nm], size: @font_manager.find_font_size(:body), leading: 6 },
+                  { text: " (", size: @font_manager.find_font_size(:body) },
+                  { text: "#{link_text}", size: @font_manager.find_font_size(:body), leading: 6,
+                    styles: [:underline], color: "888888", link: link_url },
+                  { text: ")", size: @font_manager.find_font_size(:body) },
+                ],
+                { indent_paragraphs: 0 }
+              )
+            else
+              @doc_writer.write_formatted_text(
+                doc,
+                [
+                  { text: spi[:company_nm], size: @font_manager.find_font_size(:body), leading: 6 },
+                  { text: " ", size: @font_manager.find_font_size(:body) },
+                  { text: project, size: @font_manager.find_font_size(:body), leading: 6 }
+                ],
+                { indent_paragraphs: 0 }
+              )
+            end
+
+            @layout_arranger.v_space(doc, 2)
+
+            what_n_details_list = spi[:project][project]
+            what_n_details_list.each do |what_n_details|
+              what_n_details.each_key {|what|
+                @doc_writer.indent(doc, doc.width_of("      ")) do
+                  @doc_writer.write_text(
+                    doc,
+                    what,
+                    {
+                      size: @font_manager.find_font_size(:body),
+                      leading: 6,
+                      indent_paragraphs: 0
+                    }
+                  )
+                end if what != :EMPTY_WHAT
+                details = what_n_details[what]
+                details.each do |detail_item|
+                  @doc_writer.indent(doc, doc.width_of("      ")) do
+                    @doc_writer.write_text(
+                      doc,
+                      "- #{detail_item}",
+                      {
+                        size: @font_manager.find_font_size(:body),
+                        leading: 6,
+                        indent_paragraphs: 0
+                      }
+                    )
+                  end
+                  @layout_arranger.v_space(doc, 2)
+                end
+                @layout_arranger.v_space(doc, 2)
+                @layout_arranger.v_space(doc, 2)
+                @layout_arranger.v_space(doc, 2)
+              }
+            end
+          end
+        end
+      end
+
+      @layout_arranger.v_space(doc, 2)
+      @layout_arranger.v_space(doc, 14.5)
+
+      [{ level: 4, text: "Education" }].each do |heading|
+        @layout_arranger.draw_horizontal_rule(doc)
+        @layout_arranger.v_space(doc, 9.5)
+        line_height = 1.45
+        @doc_writer.write_text(
+          doc,
+          heading[:text],
+          {
+            size: @font_manager.find_font_size(:heading),
+            style: :bold,
+            leading: line_height * @font_manager.find_font_size(:heading)
+          }
+        )
+
+        education_info = File.readlines(File.join(File.dirname(__FILE__), *relative_path, *%W[education]))
+                             .map! { |cols|
+                               cols.split(",")
+                                   .each { |col| col.strip! }
+                             }
+
+        left_col_width = 180 # Adjust based on content and page layout needs
+        right_col_start = left_col_width + 10 # Spacing between columns
+        education_info.each do |left_text, right_text|
+          # Draw left column text
+          @doc_writer.write_text_box(
+            doc,
+            left_text,
+            {
+              size: @font_manager.find_font_size(:body),
+              at: [0, @layout_arranger.y_position(doc)],
+              width: left_col_width,
+              align: :left
+            }
           )
 
-          what_n_details_list = wi[:project][solve]
-          what_n_details_list.each do |what_n_details|
-            what_n_details.each_key {|what|
-              indent(width_of("      ")) do
-                text(
-                  what,
-                  size: FONT_SIZE[:body],
-                  leading: 6,
-                  indent_paragraphs: 0
-                )
-              end if what != :EMPTY_WHAT
-              details = what_n_details[what]
-              details.each do |detail_item|
-                indent(width_of("      ")) do
-                  text(
-                    "- #{detail_item}",
-                    size: FONT_SIZE[:body],
-                    leading: 6,
-                    indent_paragraphs: 0
-                  )
-                end
-                space_after_list_item
-              end
-              space_after_list_item
-              space_after_list_item
-              space_after_list_item
+          # Draw right column text, positioned to start at the right_col_start
+          @doc_writer.write_text_box(
+            doc,
+            right_text,
+            {
+              size: @font_manager.find_font_size(:body),
+              at: [right_col_start, @layout_arranger.y_position(doc)],
+              width: @layout_arranger.bound_width(doc) - right_col_start,
+              align: :left
             }
-          end
+          )
+
+          @layout_arranger.v_space(doc, 15) # Space between rows; adjust as needed
         end
-      end
-    end
-
-    space_after_list_item
-    space_after_paragraph
-
-    [{ level: 4, text: "Side Project" }].each do |heading|
-      draw_heading(heading, FONT_SIZE)
-
-      pp = Preproc.new
-      side_project_info = []
-      wis = pp.split_by_company(File.read(File.join(File.dirname(__FILE__), *relative_path, *%W[sideProject])))
-      wis.each do |wi|
-        side_project_info << pp.group_by_company(wi.join("\n"))
-      end
-
-      side_project_info.each do |spi|
-        spi[:project].keys.each do |project|
-
-          if project.match(/<link href='([^']*)'>([^<]*)<\/link>/)
-            link_url = Regexp.last_match(1)
-            link_text = Regexp.last_match(2)
-
-            formatted_text([
-              { text: spi[:company_nm], size: FONT_SIZE[:body], leading: 6 },
-              { text: " (", size: FONT_SIZE[:body] },
-              { text: "#{link_text}", size: FONT_SIZE[:body], leading: 6, styles: [:underline], color: "888888", link: link_url },
-              { text: ")", size: FONT_SIZE[:body] },
-            ], indent_paragraphs: 0)
-          else
-            formatted_text([
-              { text: spi[:company_nm], size: FONT_SIZE[:body], leading: 6 },
-              { text: " ", size: FONT_SIZE[:body] },
-              { text: project, size: FONT_SIZE[:body], leading: 6 }
-            ], indent_paragraphs: 0)
-          end
-
-          space_after_list_item
-
-          what_n_details_list = spi[:project][project]
-          what_n_details_list.each do |what_n_details|
-            what_n_details.each_key {|what|
-              indent(width_of("      ")) do
-                text(
-                  what,
-                  size: FONT_SIZE[:body],
-                  leading: 6,
-                  indent_paragraphs: 0
-                )
-              end if what != :EMPTY_WHAT
-              details = what_n_details[what]
-              details.each do |detail_item|
-                indent(width_of("      ")) do
-                  text(
-                    "- #{detail_item}",
-                    size: FONT_SIZE[:body],
-                    leading: 6,
-                    indent_paragraphs: 0
-                  )
-                end
-                space_after_list_item
-              end
-              space_after_list_item
-              space_after_list_item
-              space_after_list_item
-            }
-          end
-        end
-      end
-    end
-
-    space_after_list_item
-    space_after_paragraph
-
-    [{ level: 4, text: "Education" }].each do |heading|
-      draw_heading(heading, FONT_SIZE)
-
-      education_info = File.readlines(File.join(File.dirname(__FILE__), *relative_path, *%W[education]))
-                           .map! { |cols|
-                             cols.split(",")
-                                 .each { |col| col.strip! }
-                           }
-
-      left_col_width = 180 # Adjust based on content and page layout needs
-      right_col_start = left_col_width + 10 # Spacing between columns
-      education_info.each do |left_text, right_text|
-        # Draw left column text
-        text_box(
-          left_text,
-          size: FONT_SIZE[:body],
-          at: [0, cursor],
-          width: left_col_width,
-          align: :left
-        )
-
-        # Draw right column text, positioned to start at the right_col_start
-        text_box(
-          right_text,
-          size: FONT_SIZE[:body],
-          at: [right_col_start, cursor],
-          width: bounds.width - right_col_start,
-          align: :left
-        )
-
-        move_down 15 # Space between rows; adjust as needed
       end
     end
   end
