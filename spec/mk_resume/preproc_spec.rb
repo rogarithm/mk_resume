67004c14e9e9493785f4311f8e3d1fe38bbca349
sehoon.gim <sehoongim@gmail.com>
2025-03-14 17:35:41 +0900

    테스트 정리한다
     - 동일한 내용을 확인하는 테스트가 여러 개인 경우, 중복되는 테스트를
       정리한다
     - 테스트 설명과 테스트 데이터 파일명 등을 다듬어서, 어떤 역할을 하는지
       알아볼 수 있게 한다

--- spec/mk_resume/preproc_spec.rb
+++ spec/mk_resume/preproc_spec.rb
@@ -12,8 +12,8 @@ describe MkResume::Preproc do
     @pp = MkResume::Preproc.new
   end
 
-  it "여러 회사명, 일한 기간을 파싱할 수 있다" do
-    src_path_sp = File.join(TEST_DATA_DIR, *%w[company_2])
+  it "여러 회사명" do
+    src_path_sp = File.join(TEST_DATA_DIR, *%w[two_company_nm])
 
     expected = [
       ["company_nm: c1", ""],
@@ -23,8 +23,8 @@ describe MkResume::Preproc do
     expect(@pp.split_by_company(File.read(src_path_sp))).to eq(expected)
   end
 
-  it "회사명, 일한 기간을 파싱할 수 있다" do
-    src_path_sp = File.join(TEST_DATA_DIR, *%w[company_1])
+  it "회사명 하나" do
+    src_path_sp = File.join(TEST_DATA_DIR, *%w[one_company_nm])
 
     expected = {
       :company_nm => "c1"
@@ -33,8 +33,8 @@ describe MkResume::Preproc do
     expect(@pp.group_by_company(File.read(src_path_sp))).to eq(expected)
   end
 
-  it "한 일에 대해 작성한 내용을 모을 수 있다" do
-    src_path_sp = File.join(TEST_DATA_DIR, *%w[solve_1])
+  it "업무 하나에 대한 상세 내용" do
+    src_path_sp = File.join(TEST_DATA_DIR, *%w[one_task])
 
     expected = {
       "s" => [
@@ -45,8 +45,8 @@ describe MkResume::Preproc do
     expect(@pp.group_project(File.read(src_path_sp))).to eq(expected)
   end
 
-  it "한 일에 대해 작성한 여러 내용을 모을 수 있다" do
-    src_path_sp = File.join(TEST_DATA_DIR, *%w[solve_2])
+  it "업무 둘에 대한 상세 내용" do
+    src_path_sp = File.join(TEST_DATA_DIR, *%w[two_task])
 
     expected = {
       "s" => [
@@ -58,7 +58,7 @@ describe MkResume::Preproc do
     expect(@pp.group_project(File.read(src_path_sp))).to eq(expected)
   end
 
-  it "여러 일에 대해 작성한 내용을 모을 수 있다" do
+  it "두 프로젝트, 프로젝트별 업무가 하나" do
     src_path_sp = File.join(TEST_DATA_DIR, *%w[solves_1])
 
     expected = {
@@ -73,7 +73,7 @@ describe MkResume::Preproc do
     expect(@pp.group_project(File.read(src_path_sp))).to eq(expected)
   end
 
-  it "여러 일에 대해 작성한 여러 내용을 모을 수 있다" do
+  it "두 프로젝트, 프로젝트당 업무가 여러 개" do
     src_path_sp = File.join(TEST_DATA_DIR, *%w[solves_2])
 
     expected = {
@@ -90,38 +90,8 @@ describe MkResume::Preproc do
     expect(@pp.group_project(File.read(src_path_sp))).to eq(expected)
   end
 
-  it "회사명, 일한 기간, 한 일을 파싱할 수 있다" do
-    src_path_sp = File.join(TEST_DATA_DIR, *%w[company_with_solve_1])
-
-    expected = {
-      :company_nm => "c1 (f ~ t)",
-      :project => {
-        "s" => [
-          { "w" => ["d1", "d2", "d3"] }
-        ]
-      }
-    }
-
-    expect(@pp.group_by_company(File.read(src_path_sp))).to eq(expected)
-  end
-
-  it "회사명, 일한 기간, 한 일을 파싱할 수 있다" do
-    src_path_sp = File.join(TEST_DATA_DIR, *%w[company_with_solve_1])
-
-    expected = {
-      :company_nm => "c1 (f ~ t)",
-      :project => {
-        "s" => [
-          { "w" => ["d1", "d2", "d3"] }
-        ]
-      }
-    }
-
-    expect(@pp.group_by_company(File.read(src_path_sp))).to eq(expected)
-  end
-
-  it "회사명, 일한 기간, 한 일 여러 개를 파싱할 수 있다" do
-    src_path_sp = File.join(TEST_DATA_DIR, *%w[company_with_solve_2])
+  it "회사 하나에 대한 포트폴리오" do
+    src_path_sp = File.join(TEST_DATA_DIR, *%w[one_portfolio])
 
     expected = {
       :company_nm => "c1 (f ~ t)",
@@ -136,8 +106,8 @@ describe MkResume::Preproc do
     expect(@pp.group_by_company(File.read(src_path_sp))).to eq(expected)
   end
 
-  it "what이 없는 경우 기본값을 설정한다" do
-    src_path_sp = File.join(TEST_DATA_DIR, *%w[no_what])
+  it "task_desc가 없는 경우 기본값을 설정한다" do
+    src_path_sp = File.join(TEST_DATA_DIR, *%w[no_task_desc])
 
     expected = {
       :project => {
@@ -150,36 +120,8 @@ describe MkResume::Preproc do
     expect(@pp.group_by_company(File.read(src_path_sp))).to eq(expected)
   end
 
-  it "actual" do
-    src_path_sp = File.join(TEST_DATA_DIR, *%w[actual])
-
-    expected = {
-      :company_nm => "(주) 두잇 (2021.05 ~ 2022.05)",
-      :project => {
-        "건축 분야 알고리즘 기반 솔루션 개발 참여" => [
-          {
-            "트레일러 3D 모델을 보여주는 혼합현실 애플리케이션 개발 (C#, Unity, MRTK)" => [
-              "수치값으로만 확인할 수 있었던 알고리즘 연산 결과를 3D 형태로 시각화해 부재 정보를 편하게 확인",
-              "기존에 Unity 기반 2D 방식으로 구현되었으나, 시각적 제한을 개선하기 위해 혼합현실 기술을 도입",
-              "사용자 편의성을 고려해 렌더링되는 트레일러의 위치와 조작 방식을 개선하고, 작업자가 현장에서 실제 트레일러와 3D 모델을 비교해볼 수 있도록 3D 모델 확대 기능을 구현"
-            ]
-          },
-          {
-            "캐드 소프트웨어 플러그인 개발 (C#, Revit API)" => [
-              "알고리즘 연산에 필요한 입력값을 캐드 프로그램에서 입력하고 추출해 쓸 수 있도록 도와주는 플러그인",
-              "캐드 외부에서 부재의 3D 모델을 활용할 수 있도록 건축 부재의 3D 모델링 정보 추출 기능을 구현",
-              "기존 건축 설계 과정(설계도 제작) 중에 알고리즘 데이터 입력을 가능하도록 해 사용성을 개선"
-            ]
-          }
-        ]
-      }
-    }
-
-    expect(@pp.group_by_company(File.read(src_path_sp))).to eq(expected)
-  end
-
-  it "real actual" do
-    src_path_sp = File.join(TEST_DATA_DIR, *%w[real_actual])
+  it "실제 데이터로 테스트한다" do
+    src_path_sp = File.join(TEST_DATA_DIR, *%w[doit_portfolio])
 
     expected = {
       :company_nm => "(주) 두잇 (2021.05 ~ 2022.05)",
